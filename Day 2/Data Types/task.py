e9d20e847d9f62c1c7db060384b0c73db67958f7
wikuskina <wikuskina@gmail.com>
2024-09-27 17:40:50 +0100

    Commit2

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,17 @@
+#  Subscripting
+print("Hello"[-2])
+
+#  String
+print("12" + "1234")
+
+# integer
+print(123_123)
+
+# float = floating point number
+a= 3.1459
+print(a)
+
+# Boolean
+print(True)
+print(False)
+
