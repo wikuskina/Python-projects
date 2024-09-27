809f4e8a1a60d6b683e64f22919e1a776a45eb19
wikuskina <wikuskina@gmail.com>
2024-09-27 17:41:47 +0100

    Commit2

---
+++ Day 2/Mathematical Operations/task.py
@@ -0,0 +1,11 @@
+# print("My age: " + str(12))
+# print("9 / 2 = " + str(9/2)) # float
+# print(9//2) # int (no remainder)
+# print(9%2) # modulo division, showing remainder
+# print(2*3) # multiplication
+# print(2**3) # exponent
+
+print(3 * 3 + 3 / 3 - 3)
+#  print out 3 instead of 7 by putting parenthesis around 3 + 3
+print(3 * (3 + 3) / 3 - 3)
+
