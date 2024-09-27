8542b6f2b09eee5edaabc008ef9abae1eef19c94
wikuskina <wikuskina@gmail.com>
2024-09-27 17:44:58 +0100

    Commit2

---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,20 @@
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+#
+# #  to floor a number - remove all decimal points
+# print(int(bmi))
+# # rounding function to round the number, (number, number of digits)
+# print(round(bmi))
+# print(round(bmi,2))
+
+# assignment operators
+# += will add the number on the right to the original value of the variable on the left
+# and assign the new value to the variable.
+
+score = 1
+score +=30
+print(score)
+
+#  f-strings to insert a variable or an expression into a string.
+data = 59595995
+print(f"this is my data: {data}")
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,15 @@
+a = len("12345")
+b = 34
+c = 35.7
+d = False
+print(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# type casting
+print(int("123")+ int("123"))
+
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
