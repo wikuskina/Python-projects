dddc962c3962243813d818c13076189753059587
wikuskina <wikuskina@gmail.com>
2024-09-30 13:59:20 +0100

    For loop revision: range function

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,7 @@
+# Gauss Challenge - adding numbers from 1 to 100
+
+sum_num = 0
+for number in range(1, 101):
+    sum_num = sum_num + number
+
+print(f"This is a total number of numbers between 1 and 100: {sum_num}")
