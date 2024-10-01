93864f6d0d0567e22d2ff8e9fd758d71f887bb67
wikuskina <wikuskina@gmail.com>
2024-10-01 14:44:14 +0100

    Functions basic

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,17 @@
+# Functions basic
+
+def get_user_name():
+    name = input("What is your name?")
+    print("Hello, " + name)
+
+print("Hello")
+get_user_name() # calling the function
+
+
+def sum_of_two_numbers(number1, number2):
+    number3 = number1 + number2
+    print("This is a sum of two numbers: " + str(number3))
+
+sum_of_two_numbers(3,9)
+
+
