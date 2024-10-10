d3b179a62cc32224f257e99a565b24353ec519e9
wikuskina <wikuskina@gmail.com>
2024-10-10 23:04:46 +0100

    Breaking down into steps. 3 first steps: picking word, guessing letter, checking it matches

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,24 @@
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
+# Picking a random word from the list
+picked_word = random.choice(word_list)
+print(picked_word)
+
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# Ask user to guess a letter in a word
+guessed_letter = input("Guess a letter:\n").lower()
+print(guessed_letter)
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+
+# Checking if guessed letter is in the picked word
+if guessed_letter in picked_word:
+    print("Right!")
+else:
+    print("Wrong!")
