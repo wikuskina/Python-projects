3a9b0d178d6c3f160da42ca43dada0b8b68c3c4b
VS <50303995+wikuskina@users.noreply.github.com>
2024-10-20 00:18:12 +0100

    Update to the love calculator game

--- Day 8/Positional vs Keyword Arguments/love_calculator.py
+++ Day 8/Positional vs Keyword Arguments/love_calculator.py
@@ -1,7 +1,11 @@
+# take two names and compare them against words TRUE LOVE and calculate how many times they match
+# each number that matches turned to string and then concacenated
+# then turn the number into integer, e.g. 3+ 5 = 35 and this would be the score for specific names
+
+
 # words to be tested against
 test1 = ["T", "R", "U", "E"]
-test2 = ["L","O","V","E"]
-
+test2 = ["L", "O", "V", "E"]
 
 def calculate_love_score(name1, name2):
     name_to_test = (name1 + name2).upper()
@@ -10,21 +14,24 @@ def calculate_love_score(name1, name2):
     number1 = 0
     number2 = 0
 
+
     for i in name_to_test:
         if i in test1:
             number1 += 1
     print(number1)
-    
+
     for j in name_to_test:
         if j in test2:
             number2 += 1
     print(number2)
-    
-    convert1 = str(number1)
-    convert2 = str(number2)
-    convert3 = convert1 + convert2
-    final_score = int(convert3)
-    
+
+
+    score = str(number1)+str(number2)
+    print(score)
+    final_score = int(score)
+
     return final_score
+
+
 
 calculate_love_score("Angela Yu", "Jack Bauer")
