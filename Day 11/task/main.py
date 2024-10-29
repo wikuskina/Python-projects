05f9e2aa47743173a41172446e1b9438b67fdb71
VS <50303995+wikuskina@users.noreply.github.com>
2024-10-29 00:12:05 +0000

    Update main.py

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -10,7 +10,6 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 want_play = "Do you want to play a game of Blackjack? Type 'y' or 'n'."
 play_or_pass = "Type 'y' to get another card, or type 'n' to pass"
 continue_playing = "y"
-game_over = False
 
 # functions to deal cards
 def deal_player():
@@ -31,42 +30,51 @@ def another_card():
     return chosen_card3
 
 while continue_playing == "y":
+    print("-----------------------------------")
     start = input(want_play).lower()
+    game_over = False
     if start == "y":
-        while game_over == False:
-            player_cards = deal_player()
-            player_sum_cards = player_cards[0]+ player_cards[1]
-            print(f"Your cards: {player_cards}. Current score: {player_sum_cards}")
-            computer_cards = deal_computer()
-            computer_sum_cards = computer_cards[0] + computer_cards[1]
-            print(f"Computer's first card: {computer_cards[0]}.")
+        print("LET'S PLAY!")
+        print("-----------------------------------")
+        player_cards = deal_player()
+        player_sum_cards = player_cards[0]+ player_cards[1]
+        print(f"Your cards: {player_cards}. Current score: {player_sum_cards}")
+        computer_cards = deal_computer()
+        computer_sum_cards = computer_cards[0] + computer_cards[1]
+        print(f"Computer's first card: {computer_cards[0]}.")
+        if player_sum_cards == 21 and computer_sum_cards == 21:
+            print("Push! You got equal score of 21 so nobody wins")
+            game_over = True
+            break
+        elif player_sum_cards == 21 and computer_sum_cards != 21:
+            print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+            print(f"Computer's cards were: {computer_cards}")
+            game_over = True
+            break
+        elif computer_sum_cards == 21 and player_sum_cards !=21 :
+            print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+            print(f"Computer's cards were: {computer_cards}")
+            game_over = True
+            break
+        elif player_sum_cards < 21 and computer_sum_cards > 21:
+            print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+            print(f"Computer's cards were: {computer_cards}")
+            game_over = True
+            break
+        elif player_sum_cards > 21 and computer_sum_cards > 21:
+            print(f"You both lost. Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+            print(f"Computer's cards were: {computer_cards}")
+            game_over = True
+            break
+        elif player_sum_cards > 21 and computer_sum_cards < 21 :
+            print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+            print(f"Computer's cards were: {computer_cards}")
+            game_over = True
+            break
+        else:
+            # further play or pass?
             print("-----------------------------------")
-            if player_sum_cards == 21 and computer_sum_cards == 21:
-                print("Push! You got equal score of 21 so nobody wins")
-                game_over = True
-                break
-            elif player_sum_cards == 21 and computer_sum_cards != 21:
-                print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
-                print(f"Computer's cards were: {computer_cards}")
-                game_over = True
-                break
-            elif computer_sum_cards == 21 and player_sum_cards !=21 :
-                print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
-                print(f"Computer's cards were: {computer_cards}")
-                game_over = True
-                break
-            elif player_sum_cards < 21 and computer_sum_cards > 21:
-                print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
-                print(f"Computer's cards were: {computer_cards}")
-                game_over = True
-                break
-            elif computer_sum_cards < 21 and player_sum_cards > 21:
-                print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
-                print(f"Computer's cards were: {computer_cards}")
-                game_over = True
-                break
-            else:
-                # further play or pass?
+            while game_over == False:
                 more_card = input(play_or_pass).lower()
                 if more_card == 'y' and game_over == False:
                     player_one_more_card = another_card()
@@ -85,28 +93,37 @@ while continue_playing == "y":
                         print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
                         print(f"Computer's cards were: {computer_cards}")
                         game_over = True
-                    elif computer_sum_cards == 21 and player_sum_cards != 21:
-                        print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
-                        print(f"Computer's cards were: {computer_cards}")
-                        game_over = True
                     elif player_sum_cards < 21 and computer_sum_cards > 21:
                         print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
                         print(f"Computer's cards were: {computer_cards}")
                         game_over = True
-                    elif computer_sum_cards < 21 and player_sum_cards > 21:
+                    elif player_sum_cards > 21 and computer_sum_cards > 21:
+                        print(f"You both lost. Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+                        print(f"Computer's cards were: {computer_cards}")
+                        game_over = True
+                    elif player_sum_cards > 21 and computer_sum_cards < 21:
                         print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
                         print(f"Computer's cards were: {computer_cards}")
                         game_over = True
+
+
                 else:
+                    if computer_sum_cards > player_sum_cards:
+                        print(f"You lost! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+                        print(f"Computer's cards were: {computer_cards}")
+                        print(f"Your cards were: {player_cards}")
+                        game_over = True
+                        break
+                    else:
+                        print(f"You won! Your score is: {player_sum_cards}, computer score is {computer_sum_cards}")
+                        print(f"Computer's cards were: {computer_cards}")
+                        print(f"Your cards were: {player_cards}")
+                        game_over = True
+                        break
 
-                    game_over = True
-                    break
-
-
-
-    else:
-        continue_playing = "n"
-        exit()
+else:
+    continue_playing = "n"
+    exit()
 
 
 
